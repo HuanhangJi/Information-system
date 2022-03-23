7d372572112c71bd1937e066eee6c40eb5dd4cde
Sansi <jihuanhangla@gmail.com>
2022-03-23 13:45:42 +0800

    增删改

--- backend/client_management/models.py
+++ backend/client_management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    pass
+
--- backend/client_management/views.py
+++ backend/client_management/views.py
@@ -1,19 +1,67 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.shortcuts import render
+import json
+from django.db.models import Q
+from django.http import *
+import numpy as np
+import requests
+import json
+from .models import *
+import hashlib
 
-# Create your views here.
 
-## TODO 增加函数
+## TODO 增加任务函数
 def insert(request):
-    pass
+    try:
+        uid = request.GET['uid']
+        price = request.GET['price']
+        owner = request.GET['owner']
+        t = Task()
+        t.uid = uid
+        t.price = price
+        t.owner = owner
+        t.save()
+        data = {'code':200,'msg':'添加任务成功'}
+    except BaseException:
+        data = {'code':404,'msg':'添加任务失败'}
+    return JsonResponse(data)
 
-## TODO 删除函数
+
+## TODO 删除任务函数
 def delete(request):
-    pass
+    try:
+        uid = request.GET['uid']
+        if Task.objects.filter(uid=uid).exist():
+            task = Task.objects.get(uid=uid)
+            task.delete()
+            data = {'code':200,'msg':'删除成功'}
+        else:
+            data = {'code':402,'msg':'不存在删除对象'}
+    except BaseException:
+        data = {'code':404,'msg':'删除失败'}
+    return JsonResponse(data)
 
-## TODO 编辑函数
+
+## TODO 编辑任务函数
 def edit(request):
-    pass
+    try:
+        uid = request.GET['uid']
+        price = request.GET['price']
+        owner = request.GET['owner']
+        if Task.objects.filter(uid=uid).exist():
+            t = Task.objects.get(uid=uid)
+            t.price = price
+            t.owner = owner
+            t.save()
+            data = {'code': 200, 'msg': '修改成功'}
+        else:
+            data = {'code':402,'msg':'未找到该任务'}
+    except BaseException:
+        data = {'code':404,'msg':'修改失败'}
+    return JsonResponse(data)
 
-## TODO 查询函数
+
+## TODO 查询任务函数
 def query(request):
     pass
