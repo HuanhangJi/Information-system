3d0c52ae80db1db374ef54a73a4f88e8e6a2d8bf
Sansi <jihuanhangla@gmail.com>
2022-03-29 22:24:59 +0800

    1

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index,name='index'), #pro登陆路由
-
+    path('',index,name='index'), #主页路由
+    path('assignments', assignments, name='assignments')#任务市场路由
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -17,3 +17,12 @@ def index(request):
         return render(request,'index/index.html',content)
     except Exception:
         return HttpResponse('打开主页失败')
+
+
+## TODO 任务市场
+def assignments(request):
+    try:
+        content = {'info':'成功打开任务市场'}
+        return render(request,'index/assignments.html',content)
+    except Exception:
+        return HttpResponse('打开任务市场失败')
