e76f11dd7979eee5106f4e337606337c2e17107f
Sansi <jihuanhangla@gmail.com>
2022-04-14 16:43:54 +0800

    DELETE VUE part

--- vue/src/components/Form/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BasicForm } from './src/BasicForm_mogai.vue';
-export { useForm } from './src/hooks/useForm';
-export * from './src/types/form';
-export * from './src/types/index';
