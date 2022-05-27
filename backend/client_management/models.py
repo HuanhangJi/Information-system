db64461f563b467815fcf1c5896206e9f3650342
Sansi <jihuanhangla@gmail.com>
2022-05-27 16:39:49 +0800

    11

--- backend/client_management/models.py
+++ backend/client_management/models.py
@@ -51,7 +51,7 @@ class Producer(UserInfo):
 
 #钱包类
 class Wallet(models.Model):#数据库修改
-    AB_id = models.CharField(max_length=25, primary_key=True, auto_created=True)
+    # AB_id = models.CharField(max_length=25, primary_key=True, auto_created=True)
     account_id = models.CharField(max_length=20)
     account_num = models.FloatField()
     payment_password = models.CharField(max_length=50)
@@ -60,14 +60,14 @@ class Wallet(models.Model):#数据库修改
 
 #银行卡类
 class Bank_card(models.Model):
-    BC_id = models.CharField(max_length=25, primary_key=True, auto_created=True)
+    # BC_id = models.CharField(max_length=25, primary_key=True, auto_created=True)
     account_id = models.CharField(max_length=20)
     card_num = models.CharField(max_length=20)
 
 
 #充值体现记录类
 class Wallet_record(models.Model):
-    cw_id = models.CharField(max_length=25, primary_key=True, auto_created=True)
+    # cw_id = models.CharField(max_length=25, primary_key=True, auto_created=True)
     AB_id = models.CharField(max_length=25)
     cw_type = models.CharField(max_length=20)
     cw_amount = models.FloatField()
